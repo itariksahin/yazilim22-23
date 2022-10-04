61ce8effc9fa692792f5faeefb9a2f104ab09fc5
itariksahin <85902981+itariksahin@users.noreply.github.com>
2022-10-04 09:46:15 +0300

    04.10.2022 hazal kod

---
+++ 04.10.2022 hazal kod/WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, sayi3, topla;
+            sayi1 = Convert.ToInt16(textBox1.Text);
+            sayi2 = Convert.ToInt16(textBox2.Text);
+            sayi3 = Convert.ToInt16(textBox3.Text);
+            topla = sayi1 + sayi2 + sayi3;
+            label3.Text = topla.ToString();
+                
+        }
+    }
+}
---
+++ 04.10.2022 hazal kod/WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sayi1, sayi2, sayi3, sayi4, sayi5, ortalama;
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            sayi2 = Convert.ToDouble(textBox2.Text);
+            sayi3 = Convert.ToDouble(textBox3.Text);
+            sayi4 = Convert.ToDouble(textBox4.Text);
+            sayi5 = Convert.ToDouble(textBox5.Text);
+            ortalama = ((sayi1 + sayi2 + sayi3 + sayi4 + sayi5));
+            
+           
+           
+
+
+
+
+
+
+
+        }
+    }
+}
