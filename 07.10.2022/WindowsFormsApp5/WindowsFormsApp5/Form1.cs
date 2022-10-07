70a37b84bd88e7d52781ab3d5069cc53cf820cc8
itariksahin <85902981+itariksahin@users.noreply.github.com>
2022-10-07 10:32:28 +0300

    07.10.2022

---
+++ 07.10.2022/WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        double onceki_ortalama = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double ortalama = (Convert.ToDouble(textBox1.Text) + Convert.ToDouble(textBox2.Text) 
+                + Convert.ToDouble(textBox3.Text) +Convert.ToDouble(textBox4.Text) 
+                + Convert.ToDouble(textBox5.Text) ) / 5;
+            label2.Text = "Önceki ortalama:" + onceki_ortalama.ToString();
+            label3.Text = "Mevcut ortlama" + ortalama.ToString();
+            if (ortalama > onceki_ortalama) { label4.BackColor = Color.Red; }
+            else if (ortalama < onceki_ortalama) { label4.BackColor = Color.Yellow; }
+            else { label4.BackColor = Color.Blue; }
+            onceki_ortalama = ortalama;
+        }
+    }
+}
---
+++ 07.10.2022/WindowsFormsApp6/WindowsFormsApp6/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt16(textBox1.Text);
+            int sayi2 = Convert.ToInt16(textBox2.Text);
+            int sayi3 = Convert.ToInt16(textBox3.Text);
+            int gecici;
+            if (sayi1 > sayi2) { gecici = sayi2;
+                                 sayi2 = sayi1;
+                                 sayi1 = gecici;
+            }
+            if (sayi1 > sayi3) {
+                gecici = sayi3;
+                sayi3 = sayi1;
+                sayi1 = gecici;
+            }
+            if (sayi2 > sayi3) {
+                gecici = sayi2;
+                sayi2 = sayi3;
+                sayi3 = gecici;
+            }
+            textBox1.Text = sayi1.ToString();
+            textBox2.Text = sayi2.ToString();
+            textBox3.Text = sayi3.ToString();
+        }
+    }
+}
