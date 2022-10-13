7e7c3856daff0009f5cc91cc70e68f155ee339bd
itariksahin <85902981+itariksahin@users.noreply.github.com>
2022-10-13 09:24:37 +0300

    11.10.2022

---
+++ 11.10.2022/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {   int gun = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int gun = Convert.ToInt32(DateTime.Now.DayOfWeek);
+            
+            switch(gun)
+            {
+                case 1: label1.Text = "pazartesi"; break;
+                case 2: label1.Text = "salı";break;
+                case 3: label1.Text = "çarşamba";break;
+                case 4: label1.Text = "perşembe";break;
+                case 5: label1.Text = "cuma";break;
+                case 6: label1.Text = "Cumartesi";break;
+                case 7: label1.Text = "pazar";break;
+                default:label1.Text = "HATA OLUŞTU"; break;
+
+            }
+            gun = gun + 1;
+            if(gun == 9) { gun = 1; }
+        }
+    }
+}
---
+++ 11.10.2022/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { byte ay = Convert.ToByte(textBox1.Text);
+            switch (ay)
+            {
+
+                case 1: label2.Text="BU AYIN ADI:"+"Ocak"; break;
+                case 2: label2.Text = "BU AYIN ADI:" + "Şubat"; break;
+                case 3: label2.Text = "BU AYIN ADI:" + "Mart"; break;
+                case 4: label2.Text = "BU AYIN ADI:" + "Nisan"; break;
+                case 5: label2.Text = "BU AYIN ADI:" + "Mayıs"; break;
+                case 6: label2.Text = "BU AYIN ADI:" + "Haziran"; break;
+                case 7: label2.Text = "BU AYIN ADI:" + "Temmuz"; break;
+                case 8: label2.Text = "BU AYIN ADI:" + "Ağustos"; break;
+                case 9: label2.Text = "BU AYIN ADI:" + "Eylül"; break;
+                case 10: label2.Text = "BU AYIN ADI:" + "Ekim"; break;
+                case 11: label2.Text = "BU AYIN ADI:" + "Kasım"; break;
+                case 12: label2.Text = "BU AYIN ADI:" + "Aralık"; break;
+
+                default: label2.Text = "BU AYIN ADI:" + "Hata Böyle bir ay yok"; break;
+            }
+        }
+    }
+}
