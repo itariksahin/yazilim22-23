ebb50bf071a8a7062624e3b63ce844b752de07cc
itariksahin <85902981+itariksahin@users.noreply.github.com>
2022-10-13 09:42:49 +0300

    13.10.2022

---
+++ 13.10.2022/WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "Ç",
+            "D",
+            "E",
+            "F",
+            "G",
+            "Ğ",
+            "H",
+            "I",
+            "İ",
+            "J",
+            "K",
+            "L",
+            "M",
+            "N",
+            "O",
+            "Ö",
+            "P",
+            "R",
+            "S",
+            "Ş",
+            "T",
+            "U",
+            "Ü",
+            "V",
+            "Y",
+            "Z"});
+            this.comboBox1.Location = new System.Drawing.Point(111, 54);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(111, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Bul";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Harf:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 152);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 13.10.2022/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedItem)
+            {
+                case "A": MessageBox.Show("A harfi 1. harftir"); break;
+                case "B": MessageBox.Show("B harfi 2. harftir"); break;
+                case "C": MessageBox.Show("C harfi 3. harftir"); break;
+                case "Ç": MessageBox.Show("Ç harfi 4. harftir"); break;
+                case "D": MessageBox.Show("D harfi 5. harftir"); break;
+                case "E": MessageBox.Show("E harfi 6. harftir"); break;
+                case "F": MessageBox.Show("F harfi 7. harftir"); break;
+                case "G": MessageBox.Show("G harfi 8. harftir"); break;
+                case "Ğ": MessageBox.Show("Ğ harfi 9. harftir"); break;
+                case "H": MessageBox.Show("H harfi 10. harftir"); break;
+                case "I": MessageBox.Show("I harfi 11. harftir"); break;
+                case "İ": MessageBox.Show("İ harfi 12. harftir"); break;
+                case "J": MessageBox.Show("J harfi 13. harftir"); break;
+                case "K": MessageBox.Show("K harfi 14. harftir"); break;
+                case "L": MessageBox.Show("L harfi 15. harftir"); break;
+                case "M": MessageBox.Show("M harfi 16. harftir"); break;
+                case "N": MessageBox.Show("N harfi 17. harftir"); break;
+                case "O": MessageBox.Show("O harfi 18. harftir"); break;
+                case "Ö": MessageBox.Show("Ö harfi 19. harftir"); break;
+                case "P": MessageBox.Show("P harfi 20. harftir"); break;
+                case "R": MessageBox.Show("R harfi 21. harftir"); break;
+                case "S": MessageBox.Show("S harfi 22. harftir"); break;
+                case "Ş": MessageBox.Show("Ş harfi 23. harftir"); break;
+                case "T": MessageBox.Show("T harfi 24. harftir"); break;
+                case "U": MessageBox.Show("U harfi 25. harftir"); break;
+                case "Ü": MessageBox.Show("Ü harfi 26. harftir"); break;
+                case "V": MessageBox.Show("V harfi 27. harftir"); break;
+                case "Y": MessageBox.Show("Y harfi 28. harftir"); break;
+                case "Z": MessageBox.Show("Z harfi 29. harftir"); break;
+            }    
+        }
+    }
+}
