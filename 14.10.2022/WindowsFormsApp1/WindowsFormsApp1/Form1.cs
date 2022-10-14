8a196c82ae09321a9a3b8eff3dcd564c3623eac4
itariksahin <85902981+itariksahin@users.noreply.github.com>
2022-10-14 13:42:30 +0300

    14.10.2022

---
+++ 14.10.2022/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string kullanici_adi = "utkan";
+            string sifre = "berkay1453";
+            if ((kullanici_adi==textBox1.Text)&&(sifre==textBox2.Text)) 
+            { MessageBox.Show("Hoş geldiniz"); }
+            else { MessageBox.Show("tekrar deneyin!!"); }
+        }
+    }
+}
---
+++ 14.10.2022/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            byte boy = Convert.ToByte(textBox1.Text);
+            byte kilo = Convert.ToByte(textBox2.Text);
+            if(comboBox1.SelectedItem=="Erkek")
+            { if ((kilo>75) && (kilo<100) && (boy>175) && (boy<195)) 
+                { MessageBox.Show("Polis olabilir."); } else { MessageBox.Show("Polis olamaz."); } }
+            if(comboBox1.SelectedItem=="Kadın")
+            {if ((kilo > 60) && (kilo < 90) && (boy > 160) && (boy < 185))
+                { MessageBox.Show("Polis olabilir."); } else { MessageBox.Show("Polis olamaz."); } }
+        }
+    }
+}
