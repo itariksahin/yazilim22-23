36e8f883eb843c61797fe70208f20206944ff887
itariksahin <85902981+itariksahin@users.noreply.github.com>
2022-11-11 08:36:54 +0300

    04.10.2022

---
+++ 10.11.2022/sınıfornegidikdortgen/sınıfornegidikdortgen/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sınıfornegidikdortgen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dikdortgen ilk_dikdortgen = new dikdortgen(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+            label3.Text = label3.Text + ilk_dikdortgen.AlanHesap().ToString()+"Metre2";
+            label4.Text = label4.Text + ilk_dikdortgen.Cevrehesap().ToString()+"Metre";
+        }
+    }
+}
---
+++ 10.11.2022/sınıfornegidikdortgen/sınıfornegidikdortgen/dikdortgen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sınıfornegidikdortgen
+{
+    internal class dikdortgen
+    {
+        private int kenar1, kenar2;
+        public dikdortgen(int kenar1, int kenar2)
+        { 
+        this.kenar1 = kenar1;
+        this.kenar2 = kenar2;
+        }
+        public int AlanHesap()
+        {
+            int alan = kenar1 * kenar2;
+            return alan;
+        }
+        public int Cevrehesap()
+        {
+            int cevre = 2 * (kenar1 + kenar2);
+            return cevre;
+        }
+    }
+}
