1e7a29d57238a2030be05c40e70ad67b029a0efe
itariksahin <85902981+itariksahin@users.noreply.github.com>
2022-12-15 10:31:41 +0300

    15.12.2022

---
+++ 15.12.2022/statik_sinif/statik_sinif/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace statik_sinif
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OgrenciIslem oi=new OgrenciIslem();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            oi.OgrenciEkle(new Ogrenci 
+            {Numara=Convert.ToInt32(textBox1.Text),AdSoyad=textBox2.Text });
+            MessageBox.Show("Öğrenci sayısı="+OgrenciIslem.OgrenciSayisi.ToString());
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        oi.OgrenciSil(Convert.ToInt32(textBox1.Text));
+         MessageBox.Show("Öğrenci sayısı=" + OgrenciIslem.OgrenciSayisi.ToString());
+        }
+    }
+}
---
+++ 15.12.2022/statik_sinif/statik_sinif/OgrenciIslem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace statik_sinif
+{
+    public class OgrenciIslem
+    {
+        public List<Ogrenci> ogrenciler;
+        public static int OgrenciSayisi { get; set; }
+
+        static OgrenciIslem()
+        {
+            OgrenciSayisi = 0;
+            Console.WriteLine("Statik yapıcı çalıştı");
+        }
+
+        public OgrenciIslem()
+        {
+            ogrenciler = new List<Ogrenci>();
+            Console.WriteLine("Yapıcı çalıştı");
+        }
+
+        public void OgrenciEkle(Ogrenci ogr)
+        {
+            ogrenciler.Add(ogr);
+            OgrenciSayisi++;
+            Console.WriteLine("Öğrenci ekleme işlemi gerçekleşti");
+        }
+
+        public void OgrenciSil(int numara)
+        {var ogr=ogrenciler.FirstOrDefault(x=>x.Numara==numara);
+            if(ogr!=null)
+            {
+                ogrenciler.Remove(ogr);
+                OgrenciSayisi--;
+                Console.WriteLine("Öğrenci silindi :;");
+            }
+
+
+        }
+
+    }
+}
