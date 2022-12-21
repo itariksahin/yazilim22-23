b14cf7a637a89e95ab1b12478e6713c6c50eb563
itariksahin <85902981+itariksahin@users.noreply.github.com>
2022-12-21 11:42:03 +0300

    20.12.2022

---
+++ 20.12.2022/Tek_Boyutlu_Dizi/Tek_Boyutlu_Dizi/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tek_Boyutlu_Dizi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] marka = new string[225];
+        int sayac = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            marka[sayac] = textBox1.Text;
+            textBox1.Text = "";
+            sayac++;
+         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for(int i=0;i<marka.Length;i++)
+            {   if (marka[i] == null) i = 225;
+                else
+                    listBox1.Items.Add(marka[i]);
+                
+            }
+        }
+    }
+}
